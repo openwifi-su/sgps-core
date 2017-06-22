c95e450456f9b5b62f3cfb85780d7530f9ea7a35
Jan-Tarek Butt <tarek@ring0.de>
2017-06-22 22:13:51 +0200

    sgps-core.go: test get request with go language.

--- src/sgps-core.go
+++ src/sgps-core.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+    "encoding/json"
+    "log"
+    "net/http"
+    "io/ioutil"
+)
+
+type test_struct struct {
+    Test string
+}
+
+func test(rw http.ResponseWriter, req *http.Request) {
+    body, err := ioutil.ReadAll(req.Body)
+    if err != nil {
+        panic(err)
+    }
+    log.Println(string(body))
+    var t test_struct
+    err = json.Unmarshal(body, &t)
+    if err != nil {
+        panic(err)
+    }
+    log.Println(t.Test)
+}
 
 func main() {
-  fmt.Println("Hello, world")
+    http.HandleFunc("/test", test)
+    log.Fatal(http.ListenAndServe(":8082", nil))
 }
