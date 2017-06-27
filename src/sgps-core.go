47fb0d0d9d8782f11543d1b2de7e94328d61384f
Jan-Tarek Butt <tarek@ring0.de>
2017-06-27 11:19:04 +0200

    sgps-core.go: prorting old ruby code to go

--- src/sgps-core.go
+++ src/sgps-core.go
@@ -1,31 +1,58 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
-    "io/ioutil"
+  "encoding/json"
+  "log"
+  "net/http"
+  "html"
+  "regexp"
+  "strings"
+  "io/ioutil"
 )
 
 type test_struct struct {
-    Test string
+  Test string
 }
 
-func test(rw http.ResponseWriter, req *http.Request) {
-    body, err := ioutil.ReadAll(req.Body)
-    if err != nil {
-        panic(err)
+func get_loc_old(w http.ResponseWriter, req *http.Request) {
+  var str = html.EscapeString(req.URL.Path)
+  var strarr = strings.Split(str, "/")
+  str = strarr[len(strarr)-1]
+  // allow only MAC addr separated by ,
+  var validID = regexp.MustCompile(`^([[:xdigit:]]{12},){0,}[[:xdigit:]]{12}$`)
+  if validID.MatchString(str) {
+    strarr = strings.Split(str, ",")
+    for _, e := range strarr {
+      log.Println(e)
     }
-    log.Println(string(body))
-    var t test_struct
-    err = json.Unmarshal(body, &t)
-    if err != nil {
-        panic(err)
-    }
-    log.Println(t.Test)
+  }else {
+    panic(str)
+  }
+}
+
+func get_loc(rw http.ResponseWriter, req *http.Request) {
+  body, err := ioutil.ReadAll(req.Body)
+  if err != nil {
+    panic(err)
+  }
+  //DEBUG: print get request
+  log.Println(string(body))
+  var t test_struct
+  err = json.Unmarshal(body, &t)
+  if err != nil {
+    panic(err)
+  }
+  log.Println(t.Test)
 }
 
 func main() {
-    http.HandleFunc("/test", test)
-    log.Fatal(http.ListenAndServe(":8082", nil))
+  // need to handle by config
+  var path = "/test"
+  var old_path = "/api/v1/bssids/"
+  var port = "8082"
+
+  http.HandleFunc(old_path, get_loc_old)
+
+  http.HandleFunc(path, get_loc)
+  log.Fatal(http.ListenAndServe(":"+port, nil))
 }
