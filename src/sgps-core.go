d5fd7e3e93b878154f2bb2180cb03e278632aa98
Jan-Tarek Butt <tarek@ring0.de>
2017-07-11 13:35:29 +0200

    src/sgps-core.go: db, Response handler intigration
    * Add quering possitions from database
    * Response query or invalid requests

--- src/sgps-core.go
+++ src/sgps-core.go
@@ -8,26 +8,74 @@ import (
   "regexp"
   "strings"
   "io/ioutil"
+  "database/sql"
+  "fmt"
+  _ "github.com/lib/pq"
 )
 
 type test_struct struct {
   Test string
 }
 
+const (
+  DB_USER     = "<user>"
+  DB_PASSWORD = "<password>"
+  DB_NAME     = "<name>"
+)
+
+// Old position request
 func get_loc_old(w http.ResponseWriter, req *http.Request) {
+  var ret = "Input not valid! "+req.URL.Path
   var str = html.EscapeString(req.URL.Path)
+  // remove all up to the last splash
   var strarr = strings.Split(str, "/")
   str = strarr[len(strarr)-1]
-  // allow only MAC addr separated by ,
-  var validID = regexp.MustCompile(`^([[:xdigit:]]{12},){0,}[[:xdigit:]]{12}$`)
+  // allow only MAC addr without colon and separated by comma
+  var validID = regexp.MustCompile(`^([[:xdigit:]]{12},){0,}[[:xdigit:]]{12}[,]?$`)
   if validID.MatchString(str) {
     strarr = strings.Split(str, ",")
+    str = ""
+    // Escape SQL string and set upper
     for _, e := range strarr {
-      log.Println(e)
+      str = str+"'"+strings.ToUpper(e)+"',"
     }
-  }else {
-    panic(str)
+    // rm last character if it comma
+    if last := len(str) -1; last >= 0 && str[last] == ',' {
+      str = str[:last]
+    }
+    // Open DB
+    dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+    DB_USER, DB_PASSWORD, DB_NAME)
+    db, err := sql.Open("postgres", dbinfo)
+    if err != nil {
+      panic(err)
+    }
+    //TODO select quallity as well
+    q := "SELECT BSSID, LAT, LON FROM bssid WHERE BSSID IN ("+str+")"
+    rows, err := db.Query(q)
+    if err != nil {
+      panic(err)
+    }
+    defer db.Close()
+    var retarr = [][]string{}
+    for rows.Next() {
+      var BSSID string
+      var LAT string
+      var LON string
+      err = rows.Scan(&BSSID, &LAT, &LON)
+      if err != nil {
+        panic(err)
+      }
+      var tmp = []string{BSSID, LAT, LON}
+      retarr = append(retarr, tmp)
+    }
+    //TODO filter unknown bssids and request seperatly by MLS
+    //TODO save reuested informations if there are unknown e.g. signal qually, MLS stuff...
+    //TODO calculate lat/lon
+    fmt.Fprintf(w, "%q\n", retarr)
+    return
   }
+  fmt.Fprintf(w, "%q\n", ret)
 }
 
 func get_loc(rw http.ResponseWriter, req *http.Request) {
@@ -45,12 +93,14 @@ func get_loc(rw http.ResponseWriter, req *http.Request) {
   log.Println(t.Test)
 }
 
+//TODO read parameters from config
 func main() {
   // need to handle by config
   var path = "/test"
   var old_path = "/api/v1/bssids/"
   var port = "8082"
 
+  //TODO Multicore able
   http.HandleFunc(old_path, get_loc_old)
 
   http.HandleFunc(path, get_loc)
